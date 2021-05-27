14d0795dbe81fbeab58fe851ece5e7bbb712c947
FStrike <fstrike7@gmail.com>
2021-05-27 20:29:28 -0300

    cambios-settings

--- RecursosWeb/settings.py
+++ RecursosWeb/settings.py
@@ -82,10 +82,13 @@ WSGI_APPLICATION = 'RecursosWeb.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':
+        dj_database_url.config(
+        default=config('DATABASE_URL')
+    ),
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -126,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
